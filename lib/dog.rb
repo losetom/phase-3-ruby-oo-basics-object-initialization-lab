ea60b755c2db05f6f4ba37d94883700ce134ed7e
Mike Loseto <losetom1998@gmail.com>
2022-10-08 13:59:56 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,11 @@
+#Class of Dog defined (1st letter capped)
 class Dog
+  #initialize method defined that accepts argument for dog's name
+  #Breed argument passed in, will default to "Mutt" if no breed is provided
     def initialize(name, breed = "Mutt")
+      #argument for dog's name stored in @name instance variable 
       @name = name
+      #Breed argument stored in @breed instance variable 
       @breed = breed
     end
   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,8 @@
+#class of Person created (1st letter always capped)
 class Person
+  #initialize method defined w/ argument passed for person's name 
     def initialize(name)
+      #aregument for name stored in @name instance variable 
       @name = name
     end
 end
